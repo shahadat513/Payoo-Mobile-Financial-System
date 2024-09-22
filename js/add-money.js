34abc98e5d29b685ac8de6483dd49371d1c48a97
shahadat513 <shahadatsohel513@gmail.com>
2024-09-22 23:23:55 +0600

    Update the add Money Option

--- js/add-money.js
+++ js/add-money.js
@@ -1,1 +1,19 @@
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addMoney = document.getElementById('input-add-money').value;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
 
+
+    if (inputPinNumber === '123') {
+        const currentBalance = document.getElementById('current-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoney)
+        const balanceNumber = parseFloat(currentBalance)
+        const newBalance = (addMoneyNumber + balanceNumber);
+        console.log(newBalance);
+        document.getElementById('current-balance').innerText = newBalance
+
+    }
+    else {
+        alert('Invalid Pin Number')
+    }
+})
--- js/login.js
+++ js/login.js
@@ -3,14 +3,14 @@ document.getElementById('btn-login').addEventListener('click', function (event) 
     event.preventDefault();
 
     const phoneNumber = document.getElementById('phone-number').value;
-    const pinNumber= document.getElementById('pin-number').value;
-    
-    if(phoneNumber==='5'&& pinNumber==='1234'){
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
         console.log('you are logged in');
-        window.location.href='/home.html'
-        
+        window.location.href = '/home.html'
+
     }
-    else{
+    else {
         alert('Invalid Phone And Pin Number')
     }
 
